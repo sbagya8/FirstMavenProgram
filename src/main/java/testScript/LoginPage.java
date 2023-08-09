9c5375f34914c3b355f1e302fa760524fcbd9d11
bagya <sbagya8@gmail.com>
2023-08-09 07:10:24 +0000

    added modification

--- src/main/java/testScript/LoginPage.java
+++ src/main/java/testScript/LoginPage.java
@@ -1,9 +1,19 @@
 package testScript;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class LoginPage {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub  https://the-internet.herokuapp.com/
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.navigate().to("https://the-internet.herokuapp.com/");
+		WebElement srcBox= driver.findElement(By.id("APjFqb"));
 
 	}
 
